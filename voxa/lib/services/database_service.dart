ea00254616df2182b324d528afdbbe67167f8c3b
netorapg <renatoplatz11@outlook.com>
2024-10-03 19:44:00 -0300

    pequeno ajuste

--- voxa/lib/services/database_service.dart
+++ voxa/lib/services/database_service.dart
@@ -1,6 +1,7 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:voxa/aplicacao/a_item.dart';
+import 'package:voxa/dominio/item.dart';
 
 class DatabaseService {
   static final DatabaseService _instance = DatabaseService._internal();
