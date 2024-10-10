038e95f1f2da18b74f1eb7aac7cf51811323d8ed
netorapg <renatoplatz11@outlook.com>
2024-10-10 15:42:31 -0300

    remoção do dominio item

--- voxa/lib/dominio/interface/i_dao_item.dart
+++
@@ -1,9 +0,0 @@
-import 'package:voxa/dominio/dto/dto_item.dart';
-
-abstract class IDAOItem {
-  Future<DTOItem> salvar(DTOItem dto);
-  Future<DTOItem> alterar(DTOItem dto);
-  Future<bool> excluir(int id);
-  Future<DTOItem> consultarPorId(int id);
-  Future<List<DTOItem>> consultar();
-}
